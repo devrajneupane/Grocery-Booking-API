4775199e4a36d111d6261795b2f62a9173c8f785
devrajneupane <asminneupane79@gmail.com>
2024-09-11 14:28:09 +0545

    feat(error): add error handler and custom error classes

---
+++ src/error/BadRequestError.ts
@@ -0,0 +1,10 @@
+import { StatusCodes } from "http-status-codes";
+
+import { BaseError } from "./BaseError";
+
+export class BadRequestError extends BaseError {
+  constructor(message: string){
+    super(message, StatusCodes.BAD_REQUEST)
+  }
+}
+
---
+++ src/error/index.ts
@@ -0,0 +1,6 @@
+export { BadRequestError } from "./BadRequestError";
+export { BaseError } from "./BaseError";
+export { ConflictError } from "./ConflictError";
+export { NotFound } from "./NotFound";
+export { UnauthenticatedError } from "./UnauthenticatedError";
+export { UnauthorizedError } from "./UnauthorizedError";
